aba923d354fc7674f8325736e3ab8c4e2d8355cc
Emil Skog <emil@skog.io>
2025-07-22 11:02:17 +0200

    Chapter 5 complete

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{net::UdpSocket, result};
+use std::net::{Ipv4Addr, UdpSocket};
 
 use crate::BytePacketbuffer::{BytePacketBuffer, DnsPacket, DnsQuestion, QueryType, ResultCode};
 mod BytePacketbuffer;
 
-fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket, Box<dyn std::error::Error>> {
-    let server = ("8.8.8.8", 53);
-
+fn lookup(
+    qname: &str,
+    qtype: QueryType,
+    server: (Ipv4Addr, u16),
+) -> Result<DnsPacket, Box<dyn std::error::Error>> {
     let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
 
     let mut packet = DnsPacket::new();
@@ -30,21 +32,21 @@ fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket, Box<dyn std::error
 fn handle_query(socket: &UdpSocket) -> Result<(), Box<dyn std::error::Error>> {
     let mut req_buffer = BytePacketBuffer::new();
 
-    let(_, src) = socket.recv_from(&mut req_buffer.buf)?;
+    let (_, src) = socket.recv_from(&mut req_buffer.buf)?;
 
     let mut request = DnsPacket::from_buffer(&mut req_buffer)?;
 
     let mut packet = DnsPacket::new();
     packet.header.id = request.header.id;
     packet.header.recursion_desired = true;
-    packet.header.recursion_available =true;
+    packet.header.recursion_available = true;
     packet.header.response = true;
 
     if let Some(question) = request.questions.pop() {
         println!("Recieved query: {:?}", question);
 
-        if let Ok(result) = lookup(&question.name, question.qtype) {
-            packet.questions.push(question);
+        if let Ok(result) = recursive_lookup(&question.name, question.qtype) {
+            packet.questions.push(question.clone());
             packet.header.rescode = result.header.rescode;
 
             for rec in result.answers {
@@ -76,12 +78,54 @@ fn handle_query(socket: &UdpSocket) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn recursive_lookup(
+    qname: &str,
+    qtype: QueryType,
+) -> Result<DnsPacket, Box<dyn std::error::Error>> {
+    let mut ns = "198.41.0.4".parse::<Ipv4Addr>().unwrap();
+
+    loop {
+        println!("attempting lookup of {:?} {} with ns {}", qtype, qname, ns);
+
+        let ns_copy = ns;
+
+        let server = (ns_copy, 53);
+        let response = lookup(qname, qtype, server)?;
+
+        if !response.answers.is_empty() && response.header.rescode == ResultCode::NOERROR {
+            return Ok(response);
+        }
+
+        if response.header.rescode == ResultCode::NXDOMAIN {
+            return Ok(response);
+        }
+
+        if let Some(new_ns) = response.get_resolved_ns(qname) {
+            ns = new_ns;
+
+            continue;
+        }
+
+        let new_ns_name = match response.get_unresolved_ns(qname) {
+            Some(x) => x,
+            None => return Ok(response),
+        };
+
+        let recursive_response = recursive_lookup(&new_ns_name, QueryType::A)?;
+        if let Some(new_ns) = recursive_response.get_random_a() {
+            ns = new_ns;
+        } else {
+            return Ok(response);
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let socket = UdpSocket::bind(("0.0.0.0", 2053))?;
 
     loop {
         match handle_query(&socket) {
-            Ok(_) => {},
+            Ok(_) => {}
             Err(e) => eprintln!("An error occured: {}", e),
         }
     }
